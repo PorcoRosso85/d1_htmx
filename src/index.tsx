9e64e84cce28e17fd75b73c6cea9c866f448c29d
PorcoRosso85 <1.is.universe@gmail.com>
2023-10-26 09:57:52 +0900

    refactor route

--- src/index.tsx
+++ src/index.tsx
@@ -33,13 +33,7 @@ type Todo = {
 
 const app = new Hono<{ Bindings: Bindings }>();
 
-const exampleRoute = new Hono();
-const contactRoute = new Hono();
 const apiRoute = new Hono();
-const todoRoute = new Hono();
-
-contactRoute.get("/", (c) => c.text("contact"));
-exampleRoute.route("/contact", contactRoute);
 
 app.get("*", renderer);
 
@@ -61,111 +55,63 @@ app.get("/", async (c) => {
   );
 });
 
-todoRoute.post(
-  // app.post(
-  "/",
-  zValidator(
-    "form",
-    z.object({
-      title: z.string().min(1),
-    })
-  ),
-  async (c) => {
-    const { title } = c.req.valid("form");
-    const id = crypto.randomUUID();
-    await c.env.DB.prepare(`INSERT INTO todo(id, title) VALUES(?, ?);`)
-      .bind(id, title)
-      .run();
-    return c.html(<Item title={title} id={id} />);
-  }
-);
-
-todoRoute.delete("/:id", async (c) => {
-  const id = c.req.param("id");
-  await c.env.DB.prepare(`DELETE FROM todo WHERE id = ?;`).bind(id).run();
-  c.status(200);
-  return c.body(null);
-});
+const todoRoute = new Hono();
+todoRoute
+  .post(
+    // app.post(
+    "/",
+    zValidator(
+      "form",
+      z.object({
+        title: z.string().min(1),
+      })
+    ),
+    async (c) => {
+      const { title } = c.req.valid("form");
+      const id = crypto.randomUUID();
+      await c.env.DB.prepare(`INSERT INTO todo(id, title) VALUES(?, ?);`)
+        .bind(id, title)
+        .run();
+      return c.html(<Item title={title} id={id} />);
+    }
+  )
+  .delete("/:id", async (c) => {
+    const id = c.req.param("id");
+    await c.env.DB.prepare(`DELETE FROM todo WHERE id = ?;`).bind(id).run();
+    c.status(200);
+    return c.body(null);
+  });
 
 type ListItem = {
   title: string;
   id: string;
 };
 
-app.get("/htmx", async (c) => {
-  const { results } = await c.env.DB.prepare(
-    `SELECT id, title FROM list;`
-  ).all<ListItem>();
-  const listItems = results;
-  return c.html(
-    <HtmlElt>
-      <div>
-        {listItems.map((item) => {
-          return <ListItem id={item.id} title={item.title} />;
-        })}
-      </div>
-      <ScriptElt />
-    </HtmlElt>
-  );
-});
-
-// app.post("/htmx/newest", async (c) => {
-app.post(
-  "htmx/newest",
-  zValidator(
-    "form",
-    z.object({
-      title: z.string().min(1),
-    })
-  ),
-  async (c) => {
-    const { title } = c.req.valid("form");
-    const id = crypto.randomUUID();
-    await c.env.DB.prepare(`INSERT INTO list(id, title) VALUES(?, ?);`)
-      .bind(id, title)
-      .run();
-    return c.html(<Newest id={id} title={title} />);
-  }
-);
-
-app.delete("/htmx/:id", async (c) => {
-  const id = c.req.param("id");
-  await c.env.DB.prepare(`DELETE FROM list WHERE id= ?;`).bind(id).run();
-  c.status(200);
-  return c.body(null);
-});
-
-app.get("/example/contact/:id/edit", async (c) => {
-  const contactId = c.req.param("id");
-  return c.html(
-    <>
-      <ClickToEdit isEditing={true} id={contactId} />
-    </>
-  );
-});
-
-app.put("/example/contact/:id", async (c) => {
-  const contactId = c.req.param("id");
-  // TODO: db更新
-  return c.html(
-    <>
-      <ClickToEdit isEditing={false} id={""} />
-      {/* TODO: 消えるメッセージ */}
-      <p hx-delete="this" hx-trigger="load" hx-swap="outerHTML swap:0.5s">
-        submitted contactId:{contactId}
-      </p>
-    </>
-  );
-});
-
-app.put("/example/contact/activate", (c) => {
-  // チェック行, putからidを取得
-  // const checkedRowsId = c.req.
-  //
-  // idからdbを更新
-  //
-  // 更新された行のHTMLを生成
-  const updatedRowHtml = `
+const contactRoute = new Hono();
+contactRoute
+  .get("/:id/edit", async (c) => {
+    const contactId = c.req.param("id");
+    return c.html(
+      <>
+        <ClickToEdit isEditing={true} id={contactId} />
+      </>
+    );
+  })
+  .put("/:id", async (c) => {
+    const contactId = c.req.param("id");
+    // TODO: db更新
+    return c.html(
+      <>
+        <ClickToEdit isEditing={false} id={""} />
+        {/* TODO: 消えるメッセージ */}
+        <p hx-delete="this" hx-trigger="load" hx-swap="outerHTML swap:0.5s">
+          submitted contactId:{contactId}
+        </p>
+      </>
+    );
+  })
+  .put("/activate", (c) => {
+    const updatedRowHtml = `
     <tr class="activate">
       <td><input type='checkbox' name='ids' value='0'></td>
       <td>Joe Smith</td>
@@ -174,12 +120,11 @@ app.put("/example/contact/activate", (c) => {
     </tr>
   `;
 
-  return c.html(updatedRowHtml);
-});
-
-app.put("/example/contact/deactivate", (c) => {
-  // 更新された行のHTMLを生成
-  const updatedRowHtml = `
+    return c.html(updatedRowHtml);
+  })
+  .put("/deactivate", (c) => {
+    // 更新された行のHTMLを生成
+    const updatedRowHtml = `
     <tr class="deactivate">
       <td><input type='checkbox' name='ids' value='0'></td>
       <td>Joe Smith</td>
@@ -188,58 +133,34 @@ app.put("/example/contact/deactivate", (c) => {
     </tr>
   `;
 
-  return c.html(updatedRowHtml);
-});
-
-app.delete("/example/contact/1", async (c) => {
-  const returnHtml = (
-    <tr>
-      <th>deleted</th>
-    </tr>
-  );
-  return c.html(returnHtml);
-});
-
-app.put("/example/contact/1", async (c) => {
-  return c.html(<EditTarget contact={contact} />);
-});
-
-app.get("/example/contact/:id", async (c) => {
-  // return c.html(<EditTarget contact={contact} />);
-  const contactId = c.req.param("id");
-  return c.html(
-    <>
-      <ClickToEdit isEditing={false} id={contactId} />
-    </>
-  );
-});
-
-app.get("/example/img", async (c) => {
-  return c.html(<img src="https://htmx.org/img/tokyo.png"></img>);
-});
-
-app.post("/example/contact/:contactId/email", async (c) => {
-  return c.html(<FormValidation isValid={false} />);
-});
-
-app.get("example/contacts/page2", async (c) => {
-  return c.html(<InfiniteScroll x={2} />);
-});
-
-app.post(
-  "/example/contacts/search",
-  zValidator(
-    "form",
-    z.object({
-      query: z.string().min(1),
-    })
-  ),
-  async (c) => {
-    const { query } = c.req.valid("form");
-    // const query = "v";
-    return c.html(<SearchResults query={query} />);
-  }
-);
+    return c.html(updatedRowHtml);
+  })
+  .delete("/1", async (c) => {
+    const returnHtml = (
+      <tr>
+        <th>deleted</th>
+      </tr>
+    );
+    return c.html(returnHtml);
+  })
+  .put("/1", async (c) => {
+    return c.html(<EditTarget contact={contact} />);
+  })
+  .get("/:id", async (c) => {
+    // return c.html(<EditTarget contact={contact} />);
+    const contactId = c.req.param("id");
+    return c.html(
+      <>
+        <ClickToEdit isEditing={false} id={contactId} />
+      </>
+    );
+  })
+  .post("/:contactId/email", async (c) => {
+    return c.html(<FormValidation isValid={false} />);
+  })
+  .get("example/contacts/page2", async (c) => {
+    return c.html(<InfiniteScroll x={2} />);
+  });
 
 // const sessionMap = {};
 
@@ -292,49 +213,108 @@ app.get(
   }
 );
 
-app.post("/example/modal/submit", async (c) => {
-  const submitted = "submitted";
-  return c.html(<>{submitted}</>);
-});
+const modalRoute = new Hono();
+modalRoute
+  .get("/", async (c) => {
+    return c.html(<DialogCustomedCalled />);
+  })
+  .post("/submit", async (c) => {
+    const submitted = "submitted";
+    return c.html(<>{submitted}</>);
+  });
 
-app.get("/example/tabs/:tabId", async (c) => {
-  return c.html(<TabContents tab={c.req.param("tabId")} />);
-});
-
-app.get("/example", async (c) => {
-  return c.html(
-    <>
-      <script src="https://unpkg.com/htmx.org@1.9.6"></script>
-      <script src="https://unpkg.com/hyperscript.org@0.9.12"></script>
-      <ClickToEdit isEditing={false} id={"1"} /> <hr />
-      <BulkUpdate /> <hr />
-      <DeleteRow /> <hr />
-      <EditRow contacts={contacts} /> <hr />
-      <LazyLoading /> <hr />
-      <FormValidation isValid={true} /> <hr />
-      <InfiniteScroll x={1} /> <hr />
-      <ActiveSearch>
-        <SearchResults query={""} />
-      </ActiveSearch>{" "}
-      <hr />
-      <ProgressBar progress={0} /> <hr />
-      <CascadingSelects>
-        <ModelSelect maker={"audi"} />
-      </CascadingSelects>{" "}
-      <hr />
-      <DialogCustomed /> <hr />
-      <DialogInBrowser /> <hr />
-      <Tabs /> <hr />
-      <KeysTest />{" "}
-    </>
-  );
-});
-
-app.get("/example/modal", async (c) => {
-  return c.html(<DialogCustomedCalled />);
-});
+const exampleRoute = new Hono();
+exampleRoute
+  .get("/htmx", async (c) => {
+    const { results } = await c.env.DB.prepare(
+      `SELECT id, title FROM list;`
+    ).all<ListItem>();
+    const listItems = results;
+    return c.html(
+      <HtmlElt>
+        <div>
+          {listItems.map((item) => {
+            return <ListItem id={item.id} title={item.title} />;
+          })}
+        </div>
+        <ScriptElt />
+      </HtmlElt>
+    );
+  })
+  .post(
+    "htmx/newest",
+    zValidator(
+      "form",
+      z.object({
+        title: z.string().min(1),
+      })
+    ),
+    async (c) => {
+      const { title } = c.req.valid("form");
+      const id = crypto.randomUUID();
+      await c.env.DB.prepare(`INSERT INTO list(id, title) VALUES(?, ?);`)
+        .bind(id, title)
+        .run();
+      return c.html(<Newest id={id} title={title} />);
+    }
+  )
+  .delete("/htmx/:id", async (c) => {
+    const id = c.req.param("id");
+    await c.env.DB.prepare(`DELETE FROM list WHERE id= ?;`).bind(id).run();
+    c.status(200);
+    return c.body(null);
+  })
+  .post(
+    "/contacts/search",
+    zValidator(
+      "form",
+      z.object({
+        query: z.string().min(1),
+      })
+    ),
+    async (c) => {
+      const { query } = c.req.valid("form");
+      // const query = "v";
+      return c.html(<SearchResults query={query} />);
+    }
+  )
+  .get("/img", async (c) => {
+    return c.html(<img src="https://htmx.org/img/tokyo.png"></img>);
+  })
+  .get("/tabs/:tabId", async (c) => {
+    return c.html(<TabContents tab={c.req.param("tabId")} />);
+  })
+  .get("/", async (c) => {
+    return c.html(
+      <>
+        <script src="https://unpkg.com/htmx.org@1.9.6"></script>
+        <script src="https://unpkg.com/hyperscript.org@0.9.12"></script>
+        <ClickToEdit isEditing={false} id={"1"} /> <hr />
+        <BulkUpdate /> <hr />
+        <DeleteRow /> <hr />
+        <EditRow contacts={contacts} /> <hr />
+        <LazyLoading /> <hr />
+        <FormValidation isValid={true} /> <hr />
+        <InfiniteScroll x={1} /> <hr />
+        <ActiveSearch>
+          <SearchResults query={""} />
+        </ActiveSearch>{" "}
+        <hr />
+        <ProgressBar progress={0} /> <hr />
+        <CascadingSelects>
+          <ModelSelect maker={"audi"} />
+        </CascadingSelects>{" "}
+        <hr />
+        <DialogCustomed /> <hr />
+        <DialogInBrowser /> <hr />
+        <Tabs /> <hr />
+        <KeysTest />{" "}
+      </>
+    );
+  });
 
 app.route("/todo", todoRoute);
-app.route("/contact", contactRoute);
+exampleRoute.route("/contact", contactRoute);
+exampleRoute.route("/modal", modalRoute);
 app.route("/example", exampleRoute);
 export default app;
